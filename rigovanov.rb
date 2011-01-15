99265b5d36298b4ba8389c5e945e5b2f486f166a
Phil Rigovanov <philippe.rigovanov@yandex.com>
2011-01-15 08:05:20 +0300

    locals

--- rigovanov.rb
+++ rigovanov.rb
@@ -6,6 +6,7 @@ require 'rubygems'
 # Bundler.setup
 
 require 'sinatra'
+require 'sinatra/r18n'
 require 'haml'
 configure :production do
   # Configure stuff here you'll want to
@@ -15,58 +16,16 @@ configure :production do
   #       from ENV['DATABASE_URI'] (see /env route below)
 end
 
+before do
+  session[:locale] = params[:locale] if params[:locale]
+end
+
+set :default_locale, 'ru'
 
 get '/' do
-  @lang = :ru
   haml :index
 end
 
-get '/ru/*' do
-  @lang = :ru
+get '/:locale/' do
   haml :index
 end
-
-get '/en/*' do
-  @lang = :en
-  haml :index_en
-end
-
-# @@ index
-# %div.title Hello world!!!!!
-
-enable :inline_templates
-
-__END__
-
-@@ layout
-%html
-  %head
-    %title 
-      - case @lang
-      - when :ru
-        = "Моя персональная страничка"
-      - when :en
-        = "My personal page"
-    %meta{:charset => 'utf-8'}
-    %meta{:name => 'author', :content => 'Филипп Юрьевич Ригованов'}
-    %meta{:name => 'robots', :content => 'index, follow'}
-  %body
-    = yield
-    / Yandex.Metrika counter
-    %div{:style => "display:none;"}
-      :javascript
-        (function(w, c) {
-            (w[c] = w[c] || []).push(function() {
-                try {
-                    w.yaCounter3224089 = new Ya.Metrika(3224089);
-                     yaCounter3224089.clickmap(true);
-                     yaCounter3224089.trackLinks(true);
-
-                } catch(e) {}
-            });
-        })(window, 'yandex_metrika_callbacks');
-    %script{:defer => "defer", :src => "//mc.yandex.ru/metrika/watch.js", :type => "text/javascript"}
-    %noscript
-      %div{:style => "position:absolute"}
-        %img{:alt => "", :src => "//mc.yandex.ru/watch/3224089"}/
-    / /Yandex.Metrika counter
